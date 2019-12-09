ad780d7c3bebf86891c30226126e888dfaec6b75
Rolly <rolly.sanchez.26@gmail.com>
2019-12-09 18:18:44 -0500

    agregando firebase

--- src/App.js
+++ src/App.js
@@ -9,8 +9,36 @@ import ActualizarHotel from './views/admin/actualizar-hotel'
 //Vista usuario final
 import Buscador from './views/buscador'
 import DetalleHotel from './views/detalle-hotel'
+//Firebase
+// import firebase from 'firebase' // importa todo los paquetes
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import firebaseConfig from './constants/firebase-config'
 
+let db = {}
 class App extends Component {
+  constructor() {
+    super()
+    firebase.initializeApp(firebaseConfig)
+    db = firebase.firestore()
+    this.addHotel()
+  }
+
+  addHotel = hotel => {
+    db.collection('users')
+      .add({
+        first: 'Alan',
+        middle: 'Mathison',
+        last: 'Turing',
+        born: 1912,
+      })
+      .then(function(docRef) {
+        console.log('Document written with ID: ', docRef.id)
+      })
+      .catch(function(error) {
+        console.error('Error adding document: ', error)
+      })
+  }
 
   render() {
     return (
---
+++ src/constants/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD08RilZ8W2yhk1u62AjMgkJNI_IhAPHuQ',
+  authDomain: 'test-54d4d.firebaseapp.com',
+  databaseURL: 'https://test-54d4d.firebaseio.com',
+  projectId: 'test-54d4d',
+  storageBucket: 'test-54d4d.appspot.com',
+  messagingSenderId: '725601209265',
+  appId: '1:725601209265:web:72ce19b8dc9617b8964b90',
+}
+
+export default firebaseConfig
