3f136f7fbe71ffda2485836d6c58736cd08527c4
Rolly <pwnakil@MacBook-Pro-de-Rolly.local>
2019-12-09 23:36:06 -0500

    Implementando e inicializando firebase

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ import Buscador from './views/buscador'
 import DetalleHotel from './views/detalle-hotel'
 //Firebase
 // import firebase from 'firebase' // importa todo los paquetes
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/app'
 import firebaseConfig from './constants/firebase-config'
 
 let db = {}
@@ -20,24 +19,6 @@ class App extends Component {
   constructor() {
     super()
     firebase.initializeApp(firebaseConfig)
-    db = firebase.firestore()
-    this.addHotel()
-  }
-
-  addHotel = hotel => {
-    db.collection('users')
-      .add({
-        first: 'Alan',
-        middle: 'Mathison',
-        last: 'Turing',
-        born: 1912,
-      })
-      .then(function(docRef) {
-        console.log('Document written with ID: ', docRef.id)
-      })
-      .catch(function(error) {
-        console.error('Error adding document: ', error)
-      })
   }
 
   render() {
