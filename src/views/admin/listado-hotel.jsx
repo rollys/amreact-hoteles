9c6475513bc85e7577114fb38008052fe9e8d0d8
Rolly <rolly.sanchez.26@gmail.com>
2019-12-10 15:48:33 -0500

    Listando hoteles en el administrador

--- src/views/admin/listado-hotel.jsx
+++ src/views/admin/listado-hotel.jsx
@@ -1,23 +1,55 @@
 import React, { Component } from 'react'
 import AdminLayout from '../layout/admin'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { TABLE_HOTELS } from '../../constants/tables'
+import { Link } from 'react-router-dom'
+import {
+  ADMIN_HOTEL_NEW,
+  ADMIN_HOTEL_UPDATE,
+  ADMIN_HOTEL_DELETE,
+} from '../../constants/routes'
 
 class ListadoHotel extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [],
+    }
+    this.listCollections()
+  }
+
+  listCollections = () => {
+    firebase
+      .firestore()
+      .collection(TABLE_HOTELS)
+      .get()
+      .then(data => {
+        const newData = []
+        data.forEach(doc => {
+          newData.push({ ...doc.data(), id: doc.id })
+        })
+        this.setState({ data: newData })
+      })
+  }
   render() {
+    const { data } = this.state
+    console.log('data2', data)
     return (
       <AdminLayout {...this.props}>
         <div className="text-center m-4">
           <h1 className="d-inline-block">
             <span className="badge badge-secondary">LISTADO DE HOTELES</span>
           </h1>
-          <button
-            type="button"
+          <Link
             className="btn btn-outline-primary float-right"
             style={{
               marginTop: 10,
               marginBottom: 10,
-            }}>
+            }}
+            to={ADMIN_HOTEL_NEW}>
             Agregar Hotel
-          </button>
+          </Link>
         </div>
         <table className="table">
           <thead className="thead-dark">
@@ -29,45 +61,35 @@ class ListadoHotel extends Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Los Delfines</td>
-              <td>350</td>
-              <td className="text-center">
-                <button type="button" className="btn btn-warning">
-                  Editar
-                </button>
-                <button type="button" className="btn btn-danger ml-3">
-                  Eliminar
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">1</th>
-              <td>Las Palmeras</td>
-              <td>60</td>
-              <td className="text-center">
-                <button type="button" className="btn btn-warning">
-                  Editar
-                </button>
-                <button type="button" className="btn btn-danger ml-3">
-                  Eliminar
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">1</th>
-              <td>Risso</td>
-              <td>100</td>
-              <td className="text-center">
-                <button type="button" className="btn btn-warning">
-                  Editar
-                </button>
-                <button type="button" className="btn btn-danger ml-3">
-                  Eliminar
-                </button>
-              </td>
-            </tr>
+            {data.length > 0 ? (
+              data.map((item, index) => {
+                return (
+                  <tr key={item.id}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td className="text-center">
+                      <Link
+                        className="btn btn-warning"
+                        to={ADMIN_HOTEL_UPDATE(item.id)}>
+                        Editar
+                      </Link>
+                      <Link
+                        className="btn btn-danger ml-3"
+                        to={ADMIN_HOTEL_DELETE(item.id)}>
+                        Eliminar
+                      </Link>
+                    </td>
+                  </tr>
+                )
+              })
+            ) : (
+              <tr>
+                <td colSpan="4" className="text-center">
+                  <strong>No hay hoteles registrados.</strong>
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </AdminLayout>
