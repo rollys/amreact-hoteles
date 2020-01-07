b01710ff0d3126783ed2883f4f8ead0107809040
Rolly <rolly.sanchez.26@gmail.com>
2020-01-06 22:24:20 -0500

    Migrando login y buscador a hooks

--- src/views/buscador.jsx
+++ src/views/buscador.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import FrontLayout from './layout/front'
 import firebase from 'firebase/app'
@@ -6,33 +6,30 @@ import 'firebase/firestore'
 import { TABLE_HOTELS } from '../constants/tables'
 import { DETAIL_HOTEL as ROUTESDETAILHOTEL } from '../constants/routes'
 
-class Buscador extends Component {
-  constructor() {
-    super()
-    this.state = {
-      data: [],
-    }
-    this.db = firebase.firestore()
+const Buscador = props => {
+  /* constructor() {
     this.listCollections()
-    this.inputSearch = React.createRef()
-  }
+  } */
 
-  listCollections = () => {
-    this.db
-      .collection(TABLE_HOTELS)
+  const db = firebase.firestore()
+  const [data, setData] = useState([])
+  const inputSearch = useRef()
+
+  const listCollections = () => {
+    console.log('listCollections()')
+    db.collection(TABLE_HOTELS)
       .get()
       .then(data => {
         const newData = []
         data.forEach(doc => {
           newData.push({ ...doc.data(), id: doc.id })
         })
-        this.setState({ data: newData })
+        setData(newData)
       })
   }
 
-  listCollectionsSearch = key => {
-    this.db
-      .collection(TABLE_HOTELS)
+  const listCollectionsSearch = key => {
+    db.collection(TABLE_HOTELS)
       .get()
       .then(data => {
         const newData = []
@@ -41,88 +38,92 @@ class Buscador extends Component {
             newData.push({ ...doc.data(), id: doc.id })
           }
         })
-        this.setState({ data: newData })
+        setData(newData)
       })
   }
 
-  search = () => {
-    const key = this.inputSearch.current.value
+  const search = () => {
+ 
+    const key = inputSearch.current.value
+    console.log(key)
     if (key === '') {
-      this.listCollections()
+      listCollections()
     } else {
-      this.listCollectionsSearch(key)
+      listCollectionsSearch(key)
     }
   }
 
-  render() {
-    return (
-      <FrontLayout>
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            name="search"
-            ref={this.inputSearch}
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              name="btn-search"
-              onClick={this.search}>
-              Buscar
-            </button>
-          </div>
+  //listCollections()
+
+
+
+  return (
+    <FrontLayout>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          name="search"
+          ref={inputSearch}
+        />
+        <div className="input-group-append">
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            name="btn-search"
+            onClick={search}>
+            Buscar
+          </button>
         </div>
-        <div className="card-deck justify-content-center">
-          {this.state.data.map(item => {
-            return (
-              <div
-                key={item.id}
-                className="card"
-                style={{
-                  minWidth: '18rem',
-                  maxWidth: '18rem',
-                  marginBottom: 10,
-                }}>
-                <img
-                  src={item.url_image}
-                  className="card-img-top"
-                  alt={item.name}
-                  width="250"
-                  height="100"
-                />
-                <div className="card-header text-center">
-                  <strong>{item.name}</strong>
-                </div>
-                <div className="card-body">
-                  <ul className="list-group">
-                    <li className="list-group-item list-group-item-success">
-                      <strong>Ciudad:</strong> {item.city}
-                    </li>
-                    <li className="list-group-item list-group-item-warning">
-                      <strong>Dirección:</strong> {item.address}
-                    </li>
-                    <li className="list-group-item list-group-item-info">
-                      <strong>Precio:</strong> S/ {item.price}
-                    </li>
-                  </ul>
-                </div>
-                <div className="card-footer text-center">
-                  <Link
-                    type="button"
-                    className="btn btn-success"
-                    to={ROUTESDETAILHOTEL(item.id)}>
-                    Ver
-                  </Link>
-                </div>
+      </div>
+      <div className="card-deck justify-content-center">
+        {data.map(item => {
+          return (
+            <div
+              key={item.id}
+              className="card"
+              style={{
+                minWidth: '18rem',
+                maxWidth: '18rem',
+                marginBottom: 10,
+              }}>
+              <img
+                src={item.url_image}
+                className="card-img-top"
+                alt={item.name}
+                width="250"
+                height="100"
+              />
+              <div className="card-header text-center">
+                <strong>{item.name}</strong>
               </div>
-            )
-          })}
-        </div>
-      </FrontLayout>
-    )
-  }
+              <div className="card-body">
+                <ul className="list-group">
+                  <li className="list-group-item list-group-item-success">
+                    <strong>Ciudad:</strong> {item.city}
+                  </li>
+                  <li className="list-group-item list-group-item-warning">
+                    <strong>Dirección:</strong> {item.address}
+                  </li>
+                  <li className="list-group-item list-group-item-info">
+                    <strong>Precio:</strong> S/ {item.price}
+                  </li>
+                </ul>
+              </div>
+              <div className="card-footer text-center">
+                <Link
+                  type="button"
+                  className="btn btn-success"
+                  to={ROUTESDETAILHOTEL(item.id)}>
+                  Ver
+                </Link>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </FrontLayout>
+  )
 }
 
 export default Buscador
--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useRef } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import Titulo from '../components/titulo'
@@ -10,88 +10,75 @@ const INIT_STATE = {
   hasError: false,
   msgError: '',
 }
-class Login extends Component {
-  constructor(props) {
-    super(props)
-    this.state = INIT_STATE
-  }
+const Login = props => {
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextState.hasError !== this.state.hasError ||
-      nextState.msgError !== this.state.msgError
-    )
-  }
+  const [dataError, setDataError] = useState(INIT_STATE)
 
-  handleSubmit = e => {
+  const inputEmail = useRef()
+  const inputPassword = useRef()
+
+  const handleSubmit = e => {
     e.preventDefault()
     firebase
       .auth()
       .signInWithEmailAndPassword(
-        this.inputEmail.value,
-        this.inputPassword.value
+        inputEmail.current.value,
+        inputPassword.current.value
       )
       .then(r => {
-        this.setState(INIT_STATE)
-        this.setUserData({
+        setDataError(INIT_STATE)
+        setUserData({
           displayName: r.user.displayName,
           email: r.user.email,
         })
-        this.props.history.push(ROUTEADMIN)
+        props.history.push(ROUTEADMIN)
       })
       .catch(error => {
         // Handle Errors here.
         // const errorCode = error.code
         const errorMessage = error.message
-        this.setState({
+        setDataError({
           hasError: true,
           msgError: errorMessage,
         })
       })
   }
 
-  setUserData = data => {
+  const setUserData = data => {
     localStorage.setItem(DATA_USER, JSON.stringify(data))
   }
 
-  render() {
-    console.log('---> render')
-    return (
-      <div className="container card w-25 mt-5 p-2">
-        <Titulo title="INICIO DE SESIÓN" />
-        <form onSubmit={this.handleSubmit}>
-          <Error {...this.state} />
-          <div className="form-group">
-            <label htmlFor="email">Correo</label>
-            <input
-              type="email"
-              className="form-control"
-              name="email"
-              ref={input => {
-                this.inputEmail = input
-              }}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              ref={input => {
-                this.inputPassword = input
-              }}
-            />
-          </div>
-          <div className="text-center">
-            <button type="submit" className="btn btn-primary">
-              Enviar
-            </button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div className="container card w-25 mt-5 p-2">
+      <Titulo title="INICIO DE SESIÓN" />
+      <form onSubmit={handleSubmit}>
+        <Error {...dataError} />
+        <div className="form-group">
+          <label htmlFor="email">Correo</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            ref={inputEmail}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            ref={inputPassword}
+          />
+        </div>
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary">
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Login
